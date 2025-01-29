341705ebc9dd6527cd2d0019370c29787f60b6ac
vishalicious213 <vishalicious213@gmail.com>
2025-01-29 13:27:20 -0800

    rendering dessert names

--- index.js
+++ index.js
@@ -1,5 +1,23 @@
-console.log("PRODUCT LIST WITH CART")
-
 import desserts from "/data.js"
 
+const dessertsSection = document.getElementById("desserts")
+
+// ⬇️ EVENT LISTENERS ⬇️
+
+// ⬇️ EVENT HANDLERS ⬇️
+
+// ⬇️ RENDER FUNCTIONS ⬇️
+
+function renderDesserts() {
+    dessertsSection.innerHTML = ""
+
+    const dessertsToRender = desserts.map(dessert => `
+        <h3>${dessert.name}<h3>
+    `).join("")
+
+    dessertsSection.innerHTML = dessertsToRender
+}
+
+
 console.log(desserts)
+renderDesserts()
