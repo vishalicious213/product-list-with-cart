ad43700c3099a073fbd66bf1b7c07daf501052e2
vishalicious213 <vishalicious213@gmail.com>
2025-01-29 13:54:30 -0800

    added id to desserts array items & id's to buttons

--- data.js
+++ data.js
@@ -7,6 +7,7 @@ export default
             "tablet": "img/image-waffle-tablet.jpg",
             "desktop": "img/image-waffle-desktop.jpg"
        },
+       "id": 0,
        "name": "Waffle with Berries",
        "category": "Waffle",
        "price": 6.50
@@ -18,6 +19,7 @@ export default
             "tablet": "img/image-creme-brulee-tablet.jpg",
             "desktop": "img/image-creme-brulee-desktop.jpg"
         },
+        "id": 1,
         "name": "Vanilla Bean Crème Brûlée",
         "category": "Crème Brûlée",
         "price": 7.00
@@ -29,6 +31,7 @@ export default
             "tablet": "img/image-macaron-tablet.jpg",
             "desktop": "img/image-macaron-desktop.jpg"
         },
+        "id": 2,
         "name": "Macaron Mix of Five",
         "category": "Macaron",
         "price": 8.00
@@ -40,6 +43,7 @@ export default
             "tablet": "img/image-tiramisu-tablet.jpg",
             "desktop": "img/image-tiramisu-desktop.jpg"
         },
+        "id": 3,
         "name": "Classic Tiramisu",
         "category": "Tiramisu",
         "price": 5.50
@@ -51,6 +55,7 @@ export default
             "tablet": "img/image-baklava-tablet.jpg",
             "desktop": "img/image-baklava-desktop.jpg"
         },
+        "id": 4,
         "name": "Pistachio Baklava",
         "category": "Baklava",
         "price": 4.00
@@ -62,6 +67,7 @@ export default
             "tablet": "img/image-meringue-tablet.jpg",
             "desktop": "img/image-meringue-desktop.jpg"
         },
+        "id": 5,
         "name": "Lemon Meringue Pie",
         "category": "Pie",
         "price": 5.00
@@ -73,6 +79,7 @@ export default
             "tablet": "img/image-cake-tablet.jpg",
             "desktop": "img/image-cake-desktop.jpg"
         },
+        "id": 6,
         "name": "Red Velvet Cake",
         "category": "Cake",
         "price": 4.50
@@ -84,6 +91,7 @@ export default
             "tablet": "img/image-brownie-tablet.jpg",
             "desktop": "img/image-brownie-desktop.jpg"
         },
+        "id": 7,
         "name": "Salted Caramel Brownie",
         "category": "Brownie",
         "price": 4.50
@@ -95,6 +103,7 @@ export default
             "tablet": "img/image-panna-cotta-tablet.jpg",
             "desktop": "img/image-panna-cotta-desktop.jpg"
         },
+        "id": 8,
         "name": "Vanilla Panna Cotta",
         "category": "Panna Cotta",
         "price": 6.50
