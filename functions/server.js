9eb2adb0880a57948f7ee79f388999b627c21fcd
vishalicious213 <vishalicious213@gmail.com>
2025-02-06 12:19:47 -0800

    drafted /functions/server.js

---
+++ functions/server.js
@@ -0,0 +1,18 @@
+exports.handler = async () => {
+    const port = process.env.PORT || 3000
+    const environment = process.env.ENVIRONMENT || "sandbox"
+    const client_id = process.env.CLIENT_ID
+    const client_secret = process.env.CLIENT_SECRET
+    const endpoint_url = environment === "sandbox" ? "https://api-m.sandbox.paypal.com" : "https://api-m.paypal.com"
+
+    console.log(client_id)
+    return {
+        statusCode: 200,
+        body: JSON.stringify({
+            port: `${port}`,
+            environment: `${environment}`,
+            client_id: `${client_id}`,
+            client_secret: `${client_secret}`
+        })
+    }
+}
